2f3f21292abdd9d581532d392211452731981c18
Pasit Sri-intarasut <s6301012620065@kmutnb.ac.th>
2022-01-26 14:53:49 +0700

    SS-7 feat: add date only column

--- SubjectDatabase.py
+++ SubjectDatabase.py
@@ -35,6 +35,7 @@ class Database:
                 CREATE TABLE subjects (
                 Id int AUTO_INCREMENT primary key NOT NULL ,
                 SubjectName VARCHAR(100), 
+                Date VARCHAR(3),
                 Time time(0),
                 Teacher VARCHAR(255),
                 Link VARCHAR(255))
@@ -49,7 +50,7 @@ class Database:
                 print(err.msg) 
 
 
-    def insert_subject(self, name, time, teacher, link):
+    def insert_subject(self, name, date, time, teacher, link):
         db = mysql.connector.connect(
                 host=self._host,
                 user=self._username,
@@ -57,8 +58,8 @@ class Database:
                 database="subject_system"
                 )
         cursor = db.cursor()
-        sql = "INSERT INTO subjects(SubjectName, Time, Teacher, Link) VALUES(%s, %s, %s, %s)"
-        val = (name , time, teacher, link)
+        sql = "INSERT INTO subjects(SubjectName, Time, Teacher, Link) VALUES(%s, %s, %s, %s, %s)"
+        val = (name , date, time, teacher, link)
         cursor.execute(sql, val)
         db.commit()
         cursor.close()
@@ -92,7 +93,7 @@ class Database:
         db.close()
         return result
 
-    def update_subject(self, sub_id, name, time, teacher, link):
+    def update_subject(self, sub_id, name, date, time, teacher, link):
         db = mysql.connector.connect(
                 host=self._host,
                 user=self._username,
@@ -100,8 +101,8 @@ class Database:
                 database="subject_system"
                 )
         cursor = db.cursor()
-        sql = "UPDATE subjects SET SubjectName=%s, Time=$s, Teacher=%s, Link=%s WHERE Id=%s"
-        val = (name, time, teacher, link, sub_id)
+        sql = "UPDATE subjects SET SubjectName=%s, Date=%s, Time=$s, Teacher=%s, Link=%s WHERE Id=%s"
+        val = (name, date, time, teacher, link, sub_id)
         cursor.execute(sql, val)
         db.commit()
         cursor.close()
@@ -125,5 +126,14 @@ if __name__ == "__main__":
     db.check_connection()
     db.create_database()
     db.create_table()
+    db.insert_subject('digital and analog', 'Mon', '9:00', 'Danucha Prasertsom', 'https://classroom.google.com/u/1/c/NDUwNzE2MzU5NjM3')
+    db.insert_subject('sofware dev', 'Mon', '13:00', 'Damrongrit Setsirichok', 'https://zoom.us/j/7505560466?pwd=SzZFblRleVVsaVE2L0YweGgwNDBVUT09')
+    db.insert_subject('physology for entorprenuor', 'Tue', '9:00', 'Prajuc Patitus', 'https://classroom.google.com/u/1/c/NDUwNzkzODcwNDIz')
+    db.insert_subject('general math', 'Tue', '13:00', 'Kanokwan Sitti...', 'https://zoom.us/rec/share/dKY4Nucemu4dm-8Ymj5djQBXaIjvcNjocP-ogtTcvGdNnwK7Ml0RpZCg4ai9m1CG.m7gvS0tO9CnbQYtp')
+    db.insert_subject('computer organization', 'Wen', '9:00', 'Danucha Prasertsom', 'https://classroom.google.com/u/1/c/NDUwNzE1NTY4OTU1')
+    db.insert_subject('Ubicom', 'Wen', '13:00', 'Yuenyong Nilsiam', ' https://zoom.us/j/6341417809?pwd=bnRLVXlMTnh0c3JaZjJSSmVQNTZvdz09')
+    db.insert_subject('Network engineer', 'Thu', '9:00', 'Natthinan Sakulpakdee', 'https://classroom.google.com/u/1/c/NDUxMjA3NTkxMjYy')
+    db.insert_subject('sofware dev', 'The', '13:00', 'Damrongrit Setsirichok', 'https://zoom.us/j/7505560466?pwd=SzZFblRleVVsaVE2L0YweGgwNDBVUT09')
+    db.select_all()
     while True:
         pass
